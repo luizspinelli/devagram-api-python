83c4f92e4e072ace24bb200c3dc7686853a7e970
Luiz Spinelli <luiz_lucchi@hotmail.com>
2022-06-27 12:38:12 -0300

    upload imgs, postagem model and classes refactor

--- middlewares/JWTMIddleware.py
+++ middlewares/JWTMIddleware.py
@@ -9,14 +9,13 @@ authService = AuthService()
 
 async def verificar_token(Authorization: str = Header(default="")):
     try:
-
         if not Authorization.split(' ')[0] == 'Bearer':
             raise HTTPException(
                 status_code=401, detail='Necessário token para autenticação')
 
         token = Authorization.split(' ')[1]
 
-        payload = AuthService.decode_jwt(token)
+        payload = AuthService.decode_jwt(AuthService, token)
 
         if not payload:
             raise HTTPException(status_code=401, detail='Token inválido')
